119a3758fbcb349bf938b0a4f4a9b3f65607ab00
shrooot <yashrajsingh977@gmail.com>
2025-06-20 11:54:53 +0530

    formatting

--- examples/rabbitmq-basic/main.go
+++ examples/rabbitmq-basic/main.go
@@ -10,19 +10,19 @@ import (
 func myFunc(queue string, args ...interface{}) error {
 	if len(args) == 0 {
 		log.Printf("Job received with no arguments")
-        return nil
-    }
+		return nil
+	}
 
 	argMap, ok := args[0].(map[string]interface{})
-    if !ok {
-        return fmt.Errorf("expected map[string]interface{}, got %T", args[0])
-    }
+	if !ok {
+		return fmt.Errorf("expected map[string]interface{}, got %T", args[0])
+	}
 
-    delete(argMap, "_aj_ruby2_keywords")
+	delete(argMap, "_aj_ruby2_keywords")
 
-    log.Printf("Job received on queue %s with arguments: %v", queue, argMap)
+	log.Printf("Job received on queue %s with arguments: %v", queue, argMap)
 
-    return nil
+	return nil
 }
 
 func init() {
@@ -35,10 +35,10 @@ func init() {
 		UseNumber:      true,
 		ExitOnComplete: false,
 		Concurrency:    2,
-		IntervalFloat: 5.0,
-		Exchange:      "activejob",
-		ExchangeType:  "direct",
-		PrefetchCount: 1,
+		IntervalFloat:  5.0,
+		Exchange:       "activejob",
+		ExchangeType:   "direct",
+		PrefetchCount:  1,
 	}
 	goworker.SetSettings(settings)
 	goworker.Register("JobSystemTestJob", myFunc)
